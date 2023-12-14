0443b60ae34cd9af9baa0a31db2b432b86b85524
toritoumi <2005toritori@gmail.com>
2023-12-14 14:20:43 -0800

    add box at center

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 500, 10000);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 150, 5000);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild(renderer.domElement);
 
-camera.position.z = 100;
+camera.position.z = 0;
+
 
 const backgroundUniverseGeometry = new THREE.BufferGeometry();
 const vertices = [];
@@ -17,11 +19,36 @@ for (let i=0;i<10000;i++){
     vertices.push(THREE.MathUtils.randFloatSpread(2000));
     vertices.push(THREE.MathUtils.randFloatSpread(2000));
 }
+
 backgroundUniverseGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
 
-const particles = new THREE.Points(backgroundUniverseGeometry, new THREE.PointsMaterial({color:0x888888}));
+const particles = new THREE.Points(backgroundUniverseGeometry, new THREE.PointsMaterial({color:0xffffff}));
 
 scene.add(particles);
+
+
+const boxGeometry = new THREE.BoxGeometry(130,130,130);
+const loader = new THREE.TextureLoader();
+
+const textures = [
+    './textures/texture1.jpg',
+    './textures/texture2.jpg',
+    './textures/texture3.jpg'
+];
+
+const boxMaterials = [];
+
+for (let i=0;i<3;i++){
+    for (let k=0;k<2;k++){
+        boxMaterials.push(new THREE.MeshBasicMaterial({map:loader.load(textures[i%3]),side:THREE.DoubleSide}));
+    }
+}
+
+const box = new THREE.Mesh(boxGeometry,boxMaterials);
+
+box.position.z = -500;
+
+scene.add(box);
 
 window.addEventListener('resize', onWindowResize,false);
 
@@ -34,6 +61,9 @@ const animate = function () {
 const render = function () {
     particles.rotation.x += 0.001;
     particles.rotation.y += 0.001;
+
+    box.rotation.x += 0.008;
+    box.rotation.y += 0.008;
 }
 
 function onWindowResize() {
